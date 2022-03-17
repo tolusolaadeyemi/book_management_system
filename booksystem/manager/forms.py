7470fdfaf689e9387d75df9810d82cb1872d591d
tolusolaadeyemi <tolusa@hotmail.com>
2022-03-17 22:45:10 +0100

    Automatic config

--- booksystem/manager/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Book,Reviews
-
-
-class AddBookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ["book_isbn","book_title","book_author","book_image","book_desc","book_genre_id"]
-
