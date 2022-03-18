a99479452ba69f53db627a5694a318ea05ba1c46
tolusolaadeyemi <tolusa@hotmail.com>
2022-03-18 11:30:05 +0100

    admin changes10

--- manager/admin.py
+++ manager/admin.py
@@ -4,15 +4,17 @@ from django.contrib import admin
 from .models import Book,Genre,Reviews
 
 
-@admin.register(Book)
-class BookAdmin(ImportExportModelAdmin,admin.ModelAdmin,resources.ModelResource):
-    list_display = ('book_title','book_author')
+class BookResource(resources.ModelResource):
     class Meta:
         model = Book
         skip_unchanged = True
         report_skipped = True
-        exclude = ('id',)
-        import_id_fields = ('book_isbn', 'book_title', 'book_author', 'book_image', 'book_desc', 'book_genre_id')
+        import_id_fields= ('book_isbn',)
+        fields = ('book_isbn', 'book_title', 'book_author', 'book_image', 'book_desc', 'book_genre_id') 
+
+@admin.register(Book)
+class BookAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display = ('book_title','book_author')
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ("genre_id","genre_name")
